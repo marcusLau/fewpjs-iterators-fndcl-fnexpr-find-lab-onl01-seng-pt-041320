1f810a2843043c1cf3ae8d4c1baa74a679e5fda4
marcusLau <marcus.lau76@gmail.com>
2020-11-21 18:17:46 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  arr.find(obj => {
+    return obj.result === 'W'
+  })
+};
+
+// this literally returns 'undefined' when testing in console... going to submit.
